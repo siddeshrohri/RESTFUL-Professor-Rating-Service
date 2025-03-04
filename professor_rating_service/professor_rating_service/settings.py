f11d7e149353a85720a717e902fc11ad441c8098
SiddeshROhri <siddeshohrri@gmail.com>
2025-03-04 13:39:12 +0000

    Updated Admin Authentication | Removed the HTML files, being redundant

--- professor_rating_service/professor_rating_service/settings.py
+++ professor_rating_service/professor_rating_service/settings.py
@@ -119,4 +119,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-LOGIN_URL = '/accounts/login/'  
+LOGIN_URL = '/admin/login/'  
